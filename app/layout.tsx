cf6a5b014c178035b7cceaf6a518568d6e310276
nok-f <nok.kurage@gmail.com>
2024-05-06 01:27:10 +0900

    feat: update home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import { ReactNode } from 'react';
 import { Metadata } from 'next';
 
-import Container from './../components/Container';
-import Header from './../components/Header';
+import Container from 'components/Container';
+import Header from 'components/Header';
 import Footer from 'components/Footer';
+
+import './../styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'さびメモ',
 };
 
-export default function RootLayout() {
+type Props = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
+    <html lang="jp">
       <body>
         <Container>
           <Header>
             <ul className="flex flex-grow w-full flex-wrap gap-x-4 gap-y-2 mb-8">
-              <li>About</li>
+              <li>Home</li>
               <li>Blogs</li>
               <li>Tags</li>
             </ul>
           </Header>
+          {children}
           <Footer />
         </Container>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <div>Hello, NextJS</div>;
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <h2 className="font-mono my-2 text-3xl font-bold">Welcome to nok.blog!</h2>
+      <section className="mt-8 mb-4">
+        <h3 className="my-2 text-2xl font-bold">このブログについて</h3>
+        <p>主に技術メモをまとめるためのブログです。</p>
+        <p>いろいろ勉強中の素人なのでなにとぞ。間違いや助言があればTwitterで教えてください。</p>
+        <p>サイトにあるコードは自己責任でご自由にお使いください。</p>
+      </section>
+      <section className="mt-8 mb-4">
+        <h3 className="my-2 text-2xl font-bold">わたしについて</h3>
+        <p>
+          普段は都内でネットワークエンジニアをしています。
+          これまでは趣味でプログラムを書いていましたが、社内システムの開発をしたいと思い、本格的に勉強を始めました。
+          主にTypescriptとGoを書いていますが、Rustにも挑戦したいと思います。XRに興味があります。
+        </p>
+      </section>
+    </>
+  );
 }
