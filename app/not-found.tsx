41f472c86e8972d121f9e6f263ad09a0d70f4ab3
nok.kurage@gmail.com <nok.kurage@gmail.com>
2024-05-23 11:08:23 +0900

    chore: mfa test

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound: React.FC = () => {
   return (
     <>
-      <h2 className="font-mono my-2 text-3xl font-bold">Not Found</h2>
+      <h2 className="font-mono my-2 text-3xl font-bold">404 Not Found</h2>
       <p>Could not find requested resource</p>
     </>
   );
